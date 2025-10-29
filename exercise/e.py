ba80fb18d7e82f27da773a71a76f6cbacf9a3841
Helenanana <lingeyon@gmail.com>
2025-10-29 19:40:46 +0800

    fetcher 1.0

--- exercise/e.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-a=pd.read_csv("binance_eth_1d_1years.csv")
-a["datetime"]=pd.to_datetime(a["datetime"])
-a.set_index("datetime", inplace=True)
-print(a)
-monthly = a['close'].resample('W').mean()
-print(monthly)
--- exercise/np.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-a=np.array([[1,2,3],[4,5,6],[7,8,9.00001]])
-# print(np.hstack((a,a)))
-print(np.linalg.det(a))
-

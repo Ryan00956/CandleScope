35bf470f8cfe3ad72907214efeb3d80fb256a12f
Helenanana <lingeyon@gmail.com>
2026-04-05 22:25:19 +0800

    right-click

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+// 禁用浏览器默认右键菜单
+document.addEventListener('contextmenu', (e) => e.preventDefault())
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
